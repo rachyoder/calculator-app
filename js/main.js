965db4cc6e1b1e06ce8ea5d4f31d5215ab21a483
Rachael Yoder <rachaelyoder@c1092933.localdomain>
2019-09-27 12:28:54 -0400

    added buttons

---
+++ js/main.js
@@ -0,0 +1,50 @@
+var app = document.getElementById("app");
+
+/* Initial Page Setup */
+
+function init() {
+    app.setAttribute("class", "container");
+    for (var i = 0; i < 3; i++) {
+        var row = document.createElement("div");
+        var col = document.createElement("div");
+        row.setAttribute("class", "row");
+        if (i == 0) {
+            col.setAttribute("class", "col-12");
+            var head = document.createElement("h2");
+            var sub = document.createElement("p");
+            head.setAttribute("class", "mx-auto turrentFont text-center display-4");
+            head.innerHTML = "CALCULATRON"
+            sub.setAttribute("class", "text-muted mx-auto text-center turrentFont display-5");
+            sub.innerHTML = "I crunch numbers... and souls"
+            col.appendChild(head);
+            col.appendChild(sub);
+            row.appendChild(col);
+        } else if (i == 1) {
+            col.setAttribute("class", "col-12 border text-white bg-dark rounded");
+            var display = document.createElement("h3");
+            display.setAttribute("class", "startFont py-2 my-0 float-right")
+            display.innerHTML = 0;
+            col.appendChild(display);
+            row.appendChild(col);
+        } else if (i == 2) {
+            var calc = document.createElement("div");
+            calc.setAttribute("class","container-fluid");
+            for (var j = 0; j < 17; j++) {
+                var button = document.createElement("div");
+                if (j == 0 || j == 1 || j == 14) {
+                    button.setAttribute("class", "col-6 border m-0 py-3 px-3 text-white bg-secondary");
+                    button.setAttribute("id", j);
+                    button.innerHTML = "x";
+                } else {
+                    button.setAttribute("class", "col-3 border m-0 py-3 py-3 text-white bg-secondary");
+                    button.setAttribute("id", j);
+                    button.innerHTML = "x";
+                }
+                calc.appendChild(button);
+                row.appendChild(calc);
+            }
+
+        }
+        app.appendChild(row); 
+    }
+}
