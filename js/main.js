bac741c03ecb124bafefca9679f587590d608268
Rachael Yoder <rachaelyoder@rachaels-mbp.lan>
2019-09-27 20:57:36 -0400

    reformatted page content

--- js/main.js
+++ js/main.js
@@ -1,15 +1,21 @@
 /* Global Variables */
 var app = document.getElementById("app");
-
+var btnContent = [
+    "Clear", "/",
+    "7", "8", "9", "*",
+    "4", "5", "6", "-",
+    "1", "2", "3", "+",
+    "0", ".", "="
+];
 /* Initial Page Setup */
 function init() {
     app.setAttribute("class", "container");
     for (var i = 0; i < 3; i++) {
         var row = document.createElement("div");
         var col = document.createElement("div");
-        row.setAttribute("class", "row");
+        row.setAttribute("class", "row mx-4");
         if (i == 0) {
-            col.setAttribute("class", "col-12");
+            col.setAttribute("class", "col-12 mt-3");
             var head = document.createElement("h2");
             var sub = document.createElement("p");
             head.setAttribute("class", "mx-auto turrentFont text-center display-5");
@@ -29,48 +35,16 @@ function init() {
         } else if (i == 2) {
             var calc = document.createElement("div");
             calc.setAttribute("class", "container");
-            for (var j = 0; j < 17; j++) {
-                console.log(j);
+            for (var j = 0; j < btnContent.length; j++) {
                 var button = document.createElement("div");
-                switch (j) {
-                    case 0:
-                        button.innerHTML = "Clear";
-                        break;
-                    case 1:
-                        button.innerHTML = "/";
-                        break;
-                    case 5:
-                        button.innerHTML = "*";
-                        break;
-                    case 9:
-                        button.innerHTML = "-";
-                        break;
-                    case 13:
-                        button.innerHTML = "+";
-                        break;
-                    case 14:
-                        button.innerHTML = "0"
-                        break;
-                    case 15:
-                        button.innerHTML = ".";
-                        break;
-                    case 16:
-                        button.innerHTML = "=";
-                        break;
-                    default:
-                        button.innerHTML = "X";
-
-                }
                 if (j == 0 || j == 1 || j == 14) {
-                    button.setAttribute("class", "col-6 border m-0 py-3 px-3 text-white bg-secondary");
-                    button.setAttribute("id", j);
+                    button.setAttribute("class", "col-6 border rounded m-0 py-3 px-3 text-center text-white bg-secondary");
                 } else {
-                    button.setAttribute("class", "col-3 border m-0 py-3 py-3 text-white bg-secondary");
-                    button.setAttribute("id", j);
-                    // button.innerHTML = "x";
+                    button.setAttribute("class", "col-3 border rounded m-0 py-3 py-3 text-center text-white bg-secondary");
                 }
+                button.innerHTML = btnContent[j];
+                button.setAttribute("id", "btn-" + btnContent[j]);
                 row.appendChild(button);
-                //row.appendChild(calc);
             }
 
         }
